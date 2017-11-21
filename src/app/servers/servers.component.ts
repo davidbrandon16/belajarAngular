4592bf5d039cbf2a50daec6cadb7394782da7871
davidbrandon16 <david.wilando4@gmail.com>
2017-11-21 16:37:03 +0700

    push yang server

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No Server was Created !";
+  serverName = '';
+  username= '';
+  canSubmit = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true ;
+    },2000);
+
+  }
+
+  ngOnInit() {
+  }
+  onCreateServer() {
+    this.serverCreationStatus ="Server was created " + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
+
+  checkCanSubmit() {
+    if(this.username != '' ) {
+      this.canSubmit = true;
+    }
+  }
+
+
+}
